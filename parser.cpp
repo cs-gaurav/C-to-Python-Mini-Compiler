b27018728b1469bc2a748306364b03172bd07fa0
cs-gaurav <gauravchuphal4444@gmail.com>
2026-03-16 18:28:00 +0530

    minor parser changes

--- parser.cpp
+++ parser.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int pos=0;
-
 void match(string next) {
     if (t[pos].type==next) pos++;
     else {
@@ -12,17 +11,23 @@ void match(string next) {
     }
 }
 
-void parse_declaration() {
+void declaration() {
     match("INT");
     match("IDENTIFIER");
-    while(t[pos].type=="COMMA") {
-        match("COMMA");
-        match("IDENTIFIER");
+    if (t[pos].type=="ASSIGN") {
+        match("ASSIGN");
+        match("NUMBER");
+    }
+    else {
+        while(t[pos].type=="COMMA") {
+            match("COMMA");
+            match("IDENTIFIER");
+        }
     }
     match("SEMI");
 }
 
-void parse_assignment() {
+void assignment() {
     match("IDENTIFIER");
     match("ASSIGN");
     match("NUMBER");
@@ -35,9 +40,9 @@ void parse_return() {
     match("SEMI");
 }
 
-void parse_statement() {
-    if(t[pos].type=="INT")    parse_declaration();
-    else if(t[pos].type=="IDENTIFIER")    parse_assignment();
+void statements() {
+    if(t[pos].type=="INT")    declaration();
+    else if(t[pos].type=="IDENTIFIER")    assignment();
     else if(t[pos].type=="RETURN")    parse_return();
     else {
         cout<<"Invalid Statement\n";
@@ -45,23 +50,23 @@ void parse_statement() {
     }
 }
 
-void parse_statement_list() {
+void inner_code() {
     while(t[pos].type!="RBRACE" && t[pos].type!="EOF") {
-        parse_statement();
+        statements();
     }
 }
 
-void parse_function() {
+void begin() {
     match("INT");
     match("IDENTIFIER");
     match("LPAREN");
     match("RPAREN");
     match("LBRACE");
-    parse_statement_list();
+    inner_code();
     match("RBRACE");
 }
 
 void parser() {
-    parse_function();
+    begin();
     if(t[pos].type!="EOF")    cout<<"Unexpected Tokens\n";
 }
