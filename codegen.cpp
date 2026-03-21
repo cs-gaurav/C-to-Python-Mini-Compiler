f030fc5081add3ac8bec94f6166cb54fe5c07c21
Panther-gehu <pranavbisht66@gmail.com>
2026-03-21 19:31:25 +0530

    Update codegen.cpp

--- codegen.cpp
+++ codegen.cpp
@@ -13,55 +13,49 @@ void generate_code() {
 
     while(t[i].type != "EOF") {
 
-        // 🔹 Skip ONLY function: int main()
+        
 if(t[i].type=="INT" && t[i+1].type=="IDENTIFIER" && t[i+2].type=="LPAREN") {
-    i += 4; // skip int main ( )
+    i += 4; 
 }
 
-        // 🔹 Declaration: int a = 7;
-        else if(t[i].type=="INT") {
-            string var = t[i+1].lexeme;
+        
+else if(t[i].type=="INT") {
+string var = t[i+1].lexeme;
 
-            if(t[i+2].type=="ASSIGN") {
-                string val = t[i+3].lexeme;
+if(t[i+2].type=="ASSIGN") {
+string val = t[i+3].lexeme;
 
-                python_code.push_back(var + " = " + val);
-                i += 5;
-            }
-            else {
-                i += 3;
-            }
-        }
+python_code.push_back(var + " = " + val);
+i += 5;
+}
+else {
+i += 3;
+}
+}
+        
+else if(t[i].type=="IDENTIFIER" && t[i+1].type=="ASSIGN") {
+string var = t[i].lexeme;
+string val = t[i+2].lexeme;
+python_code.push_back(var + " = " + val);
+i += 4;
+}
 
-        // 🔹 Assignment: a = 7;
-        else if(t[i].type=="IDENTIFIER" && t[i+1].type=="ASSIGN") {
-            string var = t[i].lexeme;
-            string val = t[i+2].lexeme;
+        
+else if(t[i].type=="RETURN") {
+string val = t[i+1].lexeme;
+python_code.push_back("print(" + val + ")");
+i += 3;
+}
 
-            python_code.push_back(var + " = " + val);
-            i += 4;
-        }
+       
+else if(t[i].type=="LBRACE" || t[i].type=="RBRACE") {
+i++;
+}
 
-        // 🔹 Return → print
-        else if(t[i].type=="RETURN") {
-            string val = t[i+1].lexeme;
-
-            python_code.push_back("print(" + val + ")");
-            i += 3;
-        }
-
-        // 🔹 Ignore braces
-        else if(t[i].type=="LBRACE" || t[i].type=="RBRACE") {
-            i++;
-        }
-
-        else {
-            i++;
-        }
-    }
-
-    // 🔥 CODE OPTIMIZATION (Dead Code Elimination)
-    // Remove statements AFTER return
+else {
+ i++;
+}
+}    
 
     vector<string> optimized;
     bool foundReturn = false;
@@ -78,7 +72,7 @@ if(t[i].type=="INT" && t[i+1].type=="IDENTIFIER" && t[i+2].type=="LPAREN") {
 
     python_code = optimized;
 
-    // 🔹 Print final Python code
+   
     cout<<"\nGenerated Python Code:\n";
     for(auto line : python_code) {
         cout<<line<<endl;
