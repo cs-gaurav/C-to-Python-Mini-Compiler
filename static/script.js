d84ece66ad9cb5f67c310557e7e119fc5198ea71
Panther-gehu <pranavbisht66@gmail.com>
2026-04-02 20:09:32 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,9 +1,11 @@
 const textarea = document.querySelector('.editor');
+
 textarea.addEventListener('keydown', function(e) {
     if (e.key === 'Tab') {
         e.preventDefault();
         const start = this.selectionStart;
         const end = this.selectionEnd;
+
         this.value = this.value.substring(0, start) + "\t" + this.value.substring(end);
         this.selectionStart = this.selectionEnd = start + 1;
     }
@@ -12,18 +14,19 @@ textarea.addEventListener('keydown', function(e) {
 function compileCode() {
     const code = document.querySelector('.editor').value;
 
-    fetch('/compile', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: 'code=' + encodeURIComponent(code)
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.open();
-        document.write(data);
-        document.close();
-    })
-    .catch(error => console.error(error));
+    
+    const form = document.createElement("form");
+    form.method = "POST";
+    form.action = "/compile";
+
+    const input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "code";
+    input.value = code;
+
+    form.appendChild(input);
+    document.body.appendChild(form);
+
+    
+    form.submit();
 }
