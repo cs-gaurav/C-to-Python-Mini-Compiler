1805931ca51197b6c91c9f10b838709a44af3777
Abhay Katiyar <abhaykat12@gmail.com>
2026-04-20 11:08:54 +0530

    updated  intr.cpp

--- inter.cpp
+++ inter.cpp
@@ -8,15 +8,28 @@ static int tempCount=1,labelCount=1;
 string newTemp(){return "t"+to_string(tempCount++);}
 string newLabel(){return "L"+to_string(labelCount++);}
 
-string expression(int &i){
-    string left;
-    if(t[i].type=="IDENTIFIER"||t[i].type=="NUMBER"){left=t[i].lexeme;i++;}
-    else{cout<<"Invalid expression\n";exit(1);}
-    while(t[i].type=="PLUS"||t[i].type=="MINUS"||t[i].type=="STAR"||t[i].type=="SLASH"||t[i].type=="MOD"){
+string factor(int &i){
+    if(t[i].type=="IDENTIFIER"||t[i].type=="NUMBER") return t[i++].lexeme;
+    else{cout<<"Invalid factor\n";exit(1);}
+}
+
+string term(int &i){
+    string left=factor(i);
+    while(t[i].type=="STAR"||t[i].type=="SLASH"||t[i].type=="MOD"){
         string op=t[i].lexeme;i++;
-        string right;
-        if(t[i].type=="IDENTIFIER"||t[i].type=="NUMBER"){right=t[i].lexeme;i++;}
-        else{cout<<"Invalid expression\n";exit(1);}
+        string right=factor(i);
+        string temp=newTemp();
+        cout<<temp<<" = "<<left<<" "<<op<<" "<<right<<endl;
+        left=temp;
+    }
+    return left;
+}
+
+string expression(int &i){
+    string left=term(i);
+    while(t[i].type=="PLUS"||t[i].type=="MINUS"){
+        string op=t[i].lexeme;i++;
+        string right=term(i);
         string temp=newTemp();
         cout<<temp<<" = "<<left<<" "<<op<<" "<<right<<endl;
         left=temp;
@@ -25,9 +38,9 @@ string expression(int &i){
 }
 
 string condition_expr(int &i){
-    string left=t[i].lexeme;i++;
+    string left=expression(i);
     string op=t[i].lexeme;i++;
-    string right=t[i].lexeme;i++;
+    string right=expression(i);
     string temp=newTemp();
     cout<<temp<<" = "<<left<<" "<<op<<" "<<right<<endl;
     return temp;
@@ -37,7 +50,11 @@ void declaration(int &i){
     i++;
     while(true){
         string name=t[i].lexeme;i++;
-        if(t[i].type=="ASSIGN"){i++;string res=expression(i);cout<<name<<" = "<<res<<endl;}
+        if(t[i].type=="ASSIGN"){
+            i++;
+            string res=expression(i);
+            cout<<name<<" = "<<res<<endl;
+        }
         if(t[i].type=="COMMA"){i++;continue;}
         break;
     }
@@ -55,37 +72,32 @@ void assignment(int &i){
     i++;
 }
 
+void statements(int &i);
+
 void if_statement(int &i){
-    i++;i++;
+    i+=2;
     string cond=condition_expr(i);
     i++;
-    string L1=newLabel(),L2=newLabel();
+    string L1=newLabel(),L2=newLabel(),L3=newLabel();
     cout<<"if "<<cond<<" goto "<<L1<<endl;
     cout<<"goto "<<L2<<endl;
     cout<<L1<<":"<<endl;
     i++;
-    while(t[i].type!="RBRACE"){
-        if(t[i].type=="IDENTIFIER")assignment(i);
-        else break;
-    }
+    while(t[i].type!="RBRACE") statements(i);
     i++;
     if(t[i].type=="ELSE"){
-        string L3=newLabel();
         cout<<"goto "<<L3<<endl;
         cout<<L2<<":"<<endl;
-        i++;i++;
-        while(t[i].type!="RBRACE"){
-            if(t[i].type=="IDENTIFIER")assignment(i);
-            else break;
-        }
+        i+=2;
+        while(t[i].type!="RBRACE") statements(i);
         i++;
         cout<<L3<<":"<<endl;
     }else cout<<L2<<":"<<endl;
 }
 
 void for_loop(int &i){
-    i++;i++;
-    if(t[i].type!="SEMI")assignment(i);
+    i+=2;
+    if(t[i].type!="SEMI") assignment(i);
     else i++;
     string L1=newLabel(),L2=newLabel();
     cout<<L1<<":"<<endl;
@@ -93,13 +105,22 @@ void for_loop(int &i){
     cout<<"ifFalse "<<cond<<" goto "<<L2<<endl;
     i++;
     int inc=i;
-    while(t[i].type!="RPAREN")i++;
+    while(t[i].type!="RPAREN") i++;
     i++;i++;
-    while(t[i].type!="RBRACE"){
-        if(t[i].type=="IDENTIFIER")assignment(i);
-        else break;
+    while(t[i].type!="RBRACE") statements(i);
+    if(t[inc].type=="IDENTIFIER"){
+        string var=t[inc].lexeme;
+        if(t[inc+1].type=="INCREMENT"){
+            string temp=newTemp();
+            cout<<temp<<" = "<<var<<" + 1"<<endl;
+            cout<<var<<" = "<<temp<<endl;
+        }
+        else if(t[inc+1].type=="DECREMENT"){
+            string temp=newTemp();
+            cout<<temp<<" = "<<var<<" - 1"<<endl;
+            cout<<var<<" = "<<temp<<endl;
+        }
     }
-    expression(inc);
     cout<<"goto "<<L1<<endl;
     cout<<L2<<":"<<endl;
     i++;
@@ -108,29 +129,32 @@ void for_loop(int &i){
 void io_statement(int &i){
     if(t[i].type=="PRINTF"){
         cout<<"print "<<t[i+2].lexeme<<endl;
-        while(t[i].type!="SEMI")i++;
+        while(t[i].type!="SEMI") i++;
         i++;
     }else{
         cout<<"input"<<endl;
-        while(t[i].type!="SEMI")i++;
+        while(t[i].type!="SEMI") i++;
         i++;
     }
+}
+
+void statements(int &i){
+    if(t[i].type=="INT") declaration(i);
+    else if(t[i].type=="IDENTIFIER") assignment(i);
+    else if(t[i].type=="IF") if_statement(i);
+    else if(t[i].type=="FOR") for_loop(i);
+    else if(t[i].type=="PRINTF"||t[i].type=="SCANF") io_statement(i);
+    else if(t[i].type=="RETURN"){
+        i++;i++;
+        if(t[i].type=="SEMI") i++;
+    }
+    else{cout<<"Invalid statement\n";exit(1);}
 }
 
 void intermediate_code(){
     int i=0;
     i+=5;
-    while(t[i].type!="EOF"&&t[i].type!="RBRACE"){
-        if(t[i].type=="INT")declaration(i);
-        else if(t[i].type=="IDENTIFIER")assignment(i);
-        else if(t[i].type=="IF")if_statement(i);
-        else if(t[i].type=="FOR")for_loop(i);
-        else if(t[i].type=="PRINTF"||t[i].type=="SCANF")io_statement(i);
-        else if(t[i].type=="RETURN"){
-            i++;i++;
-            if(t[i].type=="SEMI")i++;
-        }
-        else{cout<<"Invalid statement\n";exit(1);}
-    }
+    while(t[i].type!="EOF"&&t[i].type!="RBRACE")
+        statements(i);
     cout<<"Intermediate code generated\n";
 }
